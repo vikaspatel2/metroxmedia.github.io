67af1c21f14f6a6c204254fe9cb79f4c7f3ec9eb
Vikas Patel <vikasppatel12@gmail.com>
2022-11-24 17:06:50 +0530

    Add files via upload

---
+++ assets/chatbutton/script.js
@@ -0,0 +1,3 @@
+
+$(document).ready(function(){var a=$("#chatbutton-wa");a.floatingWhatsApp({phone:a.attr("data-phone"),popupMessage:a.attr("data-popupmessage"),headerName:a.attr("data-headername"),headerTitle:a.attr("data-headertitle"),message:a.attr("data-message"),placeholder:a.attr("data-placeholder"),showPopup:a.attr("data-showpopup"),position:a.attr("data-position"),headerColor:a.attr("data-headerColor"),autoOpenTimeout:a.attr("data-autoOpenTimeout"),size:a.attr("data-size"),buttonImage:a.attr("data-image"),
+backgroundColor:a.attr("data-backgroundColor")})});
